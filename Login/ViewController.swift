aabd2b8336e9a616214495ae5149a4ba1449409e
Fernando <fernando201196@hotmail.com>
2020-01-12 18:02:54 -0600

    Inicio del Proyecto

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Fernando Maximiliano on 12/01/20.
+//  Copyright © 2020 Fernando Maximiliano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Inicio de la aplicacion Login")
+    }
+
+
+}
+
